3f8434ee05eb1ee60cea69fa255e9f056dacb036
Marshallpro <iradukundaemmy55@gmail.com>
2026-02-02 12:33:08 +0200

    Prepare for Render deployment

--- sokohub/settings.py
+++ sokohub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s+@3y0igkhc4q$#i3+fq&!84$)lplv!uj^a)i9s=7yj#d3d3_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sokohubmini_market.com', 'www.sokohubmini_market.com','127.0.0.1' ]
 
