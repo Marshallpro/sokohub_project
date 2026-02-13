7a214154129fdbd1b3a4d2b7207942e3c7bd5842
Marshallpro <iradukundaemmy55@gmail.com>
2026-02-13 10:43:31 +0200

    testing

--- sokohub/settings.py
+++ sokohub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s+@3y0igkhc4q$#i3+fq&!84$)lplv!uj^a)i9s=7yj#d3d3_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
